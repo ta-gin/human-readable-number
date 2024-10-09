4a5424568c1c4d641a300e21f16e42153d6c8d90
ta-gin <tata.ergina@yandex.ru>
2024-10-09 20:27:17 +0300

    feat: add converting units to string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const units = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+    };
+
+    const numberToString = String(number);
+
+    function getUnits(key) {
+        return units[key];
+    }
+
+    switch (numberToString.length) {
+        case 1:
+            return getUnits(numberToString);
+    }
+};
